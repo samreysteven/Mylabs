b6e6fcc5455ffdde41a4fbf616c20d0495065c9b
Barock <aabell3@wgu.edu>
2017-03-20 19:08:15 +0700

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-# initialisasi var
-export DEBIAN_FRONTEND=noninteractive
-OS=`uname -m`;
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0'`;
-MYIP2="s/xxxxxxxxx/$MYIP/g";
+
 
 # go to root
 cd
@@ -18,6 +14,12 @@ apt-get update;apt-get -y install wget curl;
 # set time GMT +7
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
+# set repo
+wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/rizal180499/Auto-Installer-VPS/master/conf/sources.list.debian7"
+wget "http://www.dotdeb.org/dotdeb.gpg"
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
+
+
 # remove unused
 apt-get -y --purge remove samba*;
 apt-get -y --purge remove apache2*;
@@ -26,6 +28,16 @@ apt-get -y --purge remove bind9*;
 
 # update
 apt-get update; apt-get -y upgrade;
+
+# disable exim
+service exim4 stop
+sysv-rc-conf exim4 off
+
+# install badvpn
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/rizal180499/Auto-Installer-VPS/master/conf/badvpn-udpgw"
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 
 #install dropbear
 apt-get -y install dropbear
@@ -70,9 +82,20 @@ sed -i "s/ssl=1/ssl=0/g" /etc/webmin/miniserv.conf
 /etc/init.d/webmin restart
 cd
 
+# install fail2ban
+apt-get -y install fail2ban;service fail2ban restart
+
+#install squid3
+apt-get -y install squid3
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/rizal180499/Auto-Installer-VPS/master/conf/squid3.conf"
+sed -i $MYIP2 /etc/squid3/squid.conf;
+service squid3 restart
+
 service nginx start
 service php-fpm start
 service webmin restart
 service dropbear restart
 service ssh restart
+service fail2ban restart
+service squid3 restart
 rm webmin.sh
