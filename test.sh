0e0140401415c2c8456ccccae7d29d77b91f5076
Barock <aabell3@wgu.edu>
2017-04-01 12:39:25 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -20,11 +20,44 @@ wget -O https://www.mastahit.com/Debian7/user-login.sh
 wget -O https://www.mastahit.com/Debian7/user-pass.sh
 wget -O https://www.mastahit.com/Debian7/user-renew.sh
 #1
-./unshc.sh autokill.sh -o autokill.shx
-mv autokill.shx /home/vps/public_html/
+./unshc.sh dropmon.sh -o dropmon.shx
+mv dropmon.shx /home/vps/public_html/
 #2
-./unshc.sh menu.sh -o menu.shx
-mv menu.shx /home/vps/public_html/
+./unshc.sh user-active-list.sh -o user-active-list.shx
+mv user-active-list.shx /home/vps/public_html/
 #3
-./unshc.sh user-add.sh -o user-add.shx
-mv user-add.shx /home/vps/public_html/
+./unshc.sh user-add-pptp.sh -o user-add-pptp.shx
+mv user-add-pptp.shx /home/vps/public_html/
+#4
+./unshc.sh user-del.sh -o user-del.shx
+mv user-del.shx /home/vps/public_html/
+#5
+./unshc.sh disable-user-expire.sh -o disable-user-expire.shx
+mv disable-user-expire.shx /home/vps/public_html/
+#6
+./unshc.sh delete-user-expire.sh -o delete-user-expire.shx
+mv delete-user-expire.shx /home/vps/public_html/
+#1
+./unshc.sh banned-user.sh -o banned-user.shx
+mv banned-user.shx /home/vps/public_html/
+#2
+./unshc.sh user-expire-list.sh -o user-expire-list.shx
+mv user-expire-list.shx /home/vps/public_html/
+#3
+./unshc.sh user-gen.sh -o user-gen.shx
+mv user-gen.shx /home/vps/public_html/
+#4
+./unshc.sh user-limit.sh -o user-limit.shx
+mv user-limit.shx /home/vps/public_html/
+#5
+./unshc.sh user-list.sh -o user-list.shx
+mv user-list.shx /home/vps/public_html/
+#6
+./unshc.sh user-login.sh -o user-login.shx
+mv user-login.shx /home/vps/public_html/
+#1
+./unshc.sh user-pass.sh -o user-pass.shx
+mv user-pass.shx /home/vps/public_html/
+#2
+./unshc.sh user-renew.sh -o user-renew.shx
+mv user-renew.shx /home/vps/public_html/
